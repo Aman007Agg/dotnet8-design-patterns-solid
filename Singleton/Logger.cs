83537894659cf989ad5c8f51b5c431b926bad0c2
Aman007Agg <G24ait172@iitj.ac.in>
2026-06-07 17:30:39 +0530

    Singletion Pattern- Logs

---
+++ Singleton/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    internal sealed class Logger
+    {
+        private static readonly Lazy<Logger> lazyInstance = new Lazy<Logger>(() => new Logger());
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                return lazyInstance.Value;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using Singleton;
+
+class program
+{
+    static void Main()
+    {
+        Logger logger = Logger.Instance;
+        logger.Log("Application Started");
+        Logger logger2 = Logger.Instance;
+        logger.Log("Check Logs");
+        Console.WriteLine(ReferenceEquals(logger, logger2));
+    }
+}
