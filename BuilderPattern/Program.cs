0f567fc8b37d3d1a635f98226f6faea1d04cb7c8
Aman007Agg <G24ait172@iitj.ac.in>
2026-06-07 20:59:05 +0530

    Builder Pattern- Stroing user information and build

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using BuilderPattern;
+
+class Program
+{
+    static void Main()
+    {
+        User user = new UserBuilder()
+                    .SetFirstName("Aman")
+                    .SetLastName("Agrawal")
+                    .SetEmail("aman007agg@gmail.com")
+                    .SetCountry("India")
+                    .Build();
+        Console.WriteLine(
+        $"First Name: {user.FirstName}, " +
+        $"Last Name: {user.LastName}, " +
+        $"Email: {user.Email}, " +
+        $"Country: {user.Country}"
+        );
+    }
+}
---
+++ BuilderPattern/UserBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    /// <summary>
+    /// UserBuilder class is responsible for constructing a User object step by step.
+    /// It provides methods to set the properties of the User and a Build method to return the final User object.
+    /// </summary>
+    internal class UserBuilder
+    {
+        private User user;
+        public UserBuilder()
+        {
+            user = new User();
+        }
+
+        //Add Fluent Methods
+        public UserBuilder SetFirstName(string firstName)
+        {
+            user.FirstName = firstName;
+            return this;
+        }
+
+        public UserBuilder SetLastName(string lastName)
+        {
+            user.LastName = lastName;
+            return this;
+        }
+
+        public UserBuilder SetEmail(string email)
+        {
+            user.Email = email;
+            return this;
+        }
+
+        public UserBuilder SetCountry(string country)
+        {
+            user.Country = country;
+            return this;
+        }
+
+        public User Build()
+        {
+            return user;
+        }
+    }
+}
