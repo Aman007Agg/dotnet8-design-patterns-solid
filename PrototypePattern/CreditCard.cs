50d4ae0029a689a0f1d2310d88faf57ccd1cddc8
Aman007Agg <G24ait172@iitj.ac.in>
2026-06-07 21:38:04 +0530

    Protoype Pattern- Clone the existing object

---
+++ PrototypePattern/CreditCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    internal class CreditCard
+    {
+        public string CardNumber { get; set; }
+        public string UserName { get; set; }
+
+        public decimal CreditLimit { get; set; }
+
+        public CreditCard Clone()
+        {
+            return(CreditCard)this.MemberwiseClone();
+        }
+    }
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using PrototypePattern;
+
+class Program
+{
+    static void Main()
+    {
+        CreditCard card1 = new CreditCard
+        {
+            CardNumber = "1111",
+            UserName = "Aman",
+            CreditLimit = 50000
+        };
+        CreditCard card2 = card1.Clone();
+        card2.UserName = "Naman";
+
+        Console.WriteLine(card1.UserName);
+        Console.WriteLine(card2.UserName);
+    }
+}
