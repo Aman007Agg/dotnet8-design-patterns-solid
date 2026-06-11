c235118e00c0d594d1ee04b912c0a9a1ddcc4e3a
Aman007Agg <G24ait172@iitj.ac.in>
2026-06-11 09:03:22 +0530

    Tasks-async await

---
+++ Taks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Started");
+        await GetDataAsync();
+        Console.WriteLine("Finished");
+    }
+
+    static async Task GetDataAsync()
+    {
+        Console.WriteLine("Waiting..");
+        await Task.Delay(3000);
+        Console.WriteLine("Completed");
+
+    }
+}
